e910fe6e607d4287a6f0941beadc45fa80e62fef
BlackHoleMX12892 <ikerpcamposr1283456@gmail.com>
2024-01-25 15:40:52 -0600

    First commit

---
+++ about.md
@@ -0,0 +1,3 @@
+# NoMoreGames
+
+This mod deletes the no more games button!
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MainMenu ,MenuLayer) {
+	bool init() {
+		if (!MenuLayer::init())
+			return false;
+
+		auto moregamesmenu = this->getChildByID("more-games-menu");
+		auto moregamesbutton = moregamesmenu->getChildByID("more-games-button");
+
+		moregamesmenu->removeChild(moregamesbutton);
+		moregamesmenu->updateLayout();
+
+		return true;
+	}
+};
